942b45d2f4d4484df9e1e95f46d374801af94a5a
Moritz Schmitz von Hülst <msc@marketlogicsoftware.com>
2018-10-06 17:38:44 +0200

    add web3

--- service.js
+++ service.js
@@ -1,47 +1,95 @@
+require('dotenv');
 const Web3 = require('web3');
 const Tx = require('ethereumjs-tx');
 
 const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/965ee4fbd20446ce9cd9dfa60fdec01d'));
 
 const myAddress = '0xF3530d757Ce7d04346cFAfF09814BD669b532313';
-const privateKey = Buffer.from('bd4e7a6e26c7321b94354ec172f15de4aeedad96cd0435c1b0728ac7d82fb153', 'hex');
+const privateKey = Buffer.from(process.env.KEY, 'hex');
 const toAddress = '0xb63E0Ba23CE85F46e21AbD3fCD57cf6b3E38F28E';
 
-const YOUR_CONTRACT_ABI = [{"constant":false,"inputs":[{"name":"_token","type":"address"}],"name":"setToken","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"cutie","type":"uint256"},{"name":"newOwner","type":"address"}],"name":"transfer2","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}];
+const YOUR_CONTRACT_ABI = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getOwner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "newMessage",
+        "type": "string"
+      },
+      {
+        "name": "newMessage2",
+        "type": "string"
+      }
+    ],
+    "name": "setMessage",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getMessage",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
 
 //contract abi is the array that you can get from the ethereum wallet or etherscan
 const contractABI = YOUR_CONTRACT_ABI;
-const contractAddress = '0x7971e6F4F46e1A19B9d7C14628B5e68cff050198';
+const contractAddress = '0xe01a9f512082deea45c59593611e975883dcbbcc';
 //creating contract object
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 let count;
 // get transaction count, later will used as nonce
-web3.eth.getTransactionCount(myAddress).then(function (v) {
+module.exports = (callback) => web3.eth.getTransactionCount(myAddress).then(function (v) {
   console.log('Count: ' + v);
   count = v;
-  var amount = web3.utils.toHex(1e16);
-  //creating raw tranaction
+
   const rawTransaction = {
     'from': myAddress,
     'gasPrice': web3.utils.toHex(20 * 1e9),
-    'gasLimit': web3.utils.toHex(210000),
+    'gasLimit': web3.utils.toHex(60000),
     'to': contractAddress,
     'value': '0x0',
-    'data': contract.methods.transfer2(amount, toAddress).encodeABI(),
+    'data': contract.methods.setMessage('Hello HLAG 2017', 'Bazooka').encodeABI(),
     'nonce': web3.utils.toHex(count)
   };
-  console.log(rawTransaction);
-  //creating tranaction via ethereumjs-tx
-  var transaction = new Tx(rawTransaction);
-  //signing transaction with private key
-  transaction.sign(privateKey);
-  //sending transacton via web3 module
-  web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
-    .on('transactionHash', console.log);
 
-  contract.methods.isConfig.call()
-    .then(function (balance) {
-      console.log(balance);
-    });
+  const transaction = new Tx(rawTransaction);
+
+  transaction.sign(privateKey);
+
+  web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
+    .on('transactionHash', (...args) => console.log('args', args))
+    .on('receipt', () => callback());
 });
