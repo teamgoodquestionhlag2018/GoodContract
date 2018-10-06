4365fae1378530050807bf7f11bcc689feb6b138
Moritz Schmitz von Hülst <msc@marketlogicsoftware.com>
2018-10-06 14:19:42 +0200

    add dummy endpoints

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,75 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/proposals', function (req, res, next) {
+  res.send([
+    {
+      freelancer: '0x123',
+      client: '0x456',
+      milestones: [
+        {
+          prize: 123,
+          requirements: '',
+          dueDate: JSON.stringify(Date.now())
+        }
+      ]
+    },
+    {
+      freelancer: '0x123',
+      client: '0x456',
+      milestones: [
+        {
+          prize: 123,
+          requirements: '',
+          dueDate: JSON.stringify(Date.now())
+        }
+      ]
+    }
+  ]);
+});
+
+router.post('/proposals', function (req, res, next) {
+  res.send({
+    freelancer: '0x123',
+    client: '0x456',
+    milestones: [
+      {
+        prize: 123,
+        requirements: '',
+        dueDate: JSON.stringify(Date.now())
+      }
+    ]
+  });
+});
+
+router.get('/proposals/:id', function (req, res, next) {
+  res.send({
+    id: req.params.id,
+    freelancer: '0x123',
+    client: '0x456',
+    milestones: [
+      {
+        prize: 123,
+        requirements: '',
+        dueDate: JSON.stringify(Date.now())
+      }
+    ]
+  });
+});
+
+router.put('/proposals/:id/sign', function (req, res, next) {
+  res.send({
+    id: req.params.id,
+    freelancer: '0x123',
+    client: '0x456',
+    milestones: [
+      {
+        prize: 123,
+        requirements: '',
+        dueDate: JSON.stringify(Date.now())
+      }
+    ]
+  });
 });
 
 module.exports = router;
