ea5a32f1e842dd1265e35b4a7d9f2d50b07a0a98
linahanner <lina.hannersanchez@gmail.com>
2018-10-07 11:35:20 +0200

    Update user service and remove user controller

--- services/UserServices.js
+++ services/UserServices.js
@@ -1,31 +1,43 @@
 var userRepository = require('../data/mocks/MockUserRepository');
+var uuid = require('uuid/v4');
 
 class ProposalService {
     constructor() {
 
     }
 
-    GetUsers() {
-        var users = userRepository.GetUsers();
-
-        var views = users.map(GetUserView)
-
-        return views;
-    }
-    GetUserById(id) {
-        var user = userRepository.GetUserById(id);
-
-        var view = GetUserView(user);
-
-        return view;
-    }
-    AddUser(user) {
+    Register(user) {
         userRepository.AddUser(user);
+    }
+    Login(id) {
+        var data = userRepository.GetUserById(id);
+
+        if (data == null) {
+            throw {
+
+            };
+        }
+
+        data.token = uuid();
+        return data.token;
+    }
+    ValidateUser(token) {
+        var data = userRepository.GetUserByToken(token);
+
+        if (data === undefined) {
+            throw {
+                code: 401,
+                message: "Invalid token"
+            };
+        }
+
+        return GetUserView(data);
     }
 }
 
 function GetUserView(data) {
     let view = JSON.parse(JSON.stringify(data));
+    view.token = null;
 
     return view;
 }
